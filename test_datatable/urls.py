d2050c6dcfe625948d3bf2716bf704bb12e8c1be
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2023-09-10 22:27:05 +0300

    added ajax endpoint

--- test_datatable/urls.py
+++ test_datatable/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from . import ajax_datatable_views
 from . import views
 
 urlpatterns = [
     path('init_db/', views.init_db, name='init_db'),
+    path('view/', views.view_data, name='view_data'),
+    path('ajax_datatable/studies/', ajax_datatable_views.StudiesAjaxDatatableView.as_view(),
+         name="ajax_datatable_studies"),
+
 ]
